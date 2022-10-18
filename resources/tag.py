365008803c6b2a07ba1c0dbb0e2100168508095f
Alex Wu <alexwu727@gmail.com>
2022-10-17 23:42:02 -0700

    built item and tag many-to-many relationship

--- resources/tag.py
+++ resources/tag.py
@@ -4,8 +4,8 @@ from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
-from models import TagModel, StoreModel
-from schemas import TagSchema
+from models import TagModel, StoreModel, ItemModel
+from schemas import TagSchema, ItemAndTagSchema
 
 blp = Blueprint("tags", __name__, description="Operations on tags")
 
@@ -37,3 +37,40 @@ class Tag(MethodView):
     def get(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
         return tag
+
+    @blp.response(202, TagSchema, description="Deletes a tag if no item is tagged with it.", example={"message": "Tag deleted."})
+    @blp.alt_response(404, description="Tag not found.")
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return tag
+        abort(400, message="Could not delete tag. Make sure tag is not associated with any items, then try again.")
+
+
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class LinkItemToTag(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+        item.tags.append(tag)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+        return tag
+
+    @blp.response(200, ItemAndTagSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+        item.tags.remove(tag)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while removing the tag.")
+        return {"message": "Tag removed from item", "item": item, "tag": tag}
--- schemas.py
+++ schemas.py
@@ -25,11 +25,19 @@ class StoreSchema(PlainStoreSchema):
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
 
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class ItemAndTagSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
 
 
 class StoreUpdateSchema(Schema):
